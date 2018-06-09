9e30c3517ac2ee294ff47fb79eefc900c2c1b7e3
Chris Pearce <cpearce@mozilla.com>
2018-06-09 23:09:49 +1200

    Display kohi 2018 Google calendar on import page.

--- src/GoogleCalendarImport.js
+++ src/GoogleCalendarImport.js
@@ -1,12 +1,63 @@
 import React, { Component } from 'react';
 
+async function getGCal(calendarId) {
+  const api = "https://www.googleapis.com/calendar/v3/calendars/"
+  let url =
+    api + calendarId + "/events?"
+    + "key=AIzaSyBqhqI543V2ejZi6B_CniqJ7eQYq-iW0wE"
+    + "&timeMin=2018-01-01T00:00:00.000Z&maxResults=250"
+    + "&timeMax=2019-01-01T00:00:00.000Z";
+    let nextPageToken = undefined;
+    let calendar = [];
+    while (true) {
+      let pageToken = nextPageToken ? "&pageToken=" + nextPageToken : "";
+      let response = await fetch(url + pageToken, {
+        cache: "no-cache",
+      });
+      let page = await response.json();
+      calendar.push(page);
+      if (page.nextPageToken) {
+        nextPageToken = page.nextPageToken;
+      } else {
+        break;
+      }
+    }
+
+    return calendar;
+}
+
 export class GoogleCalendarImport extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      items: [],
+    };
+    getGCal('office%40kohimarama.school.nz')
+    .then((data) => {
+      console.log(data);
+      let items = data.reduce((accumulator, value) => {
+        let confirmed = value.items.filter(i => i.status === "confirmed");
+        return accumulator.concat(confirmed);
+      }, []);
+      items.sort((a,b) => a.start.date.localeCompare(b.start.date));
+      this.setState({items: items});
+      console.log(items);
+    },(err) => {
+      console.log(err);
+    });
   }
   render() {
+    let items = this.state.items.map((item) => {
+      return (
+        <div key={item.id} className="gcal-item">
+          {item.start.date} - {item.end.date} ; {item.summary}
+        </div>
+      );
+    });
     return (
       <div>
+        <p>office@kohimarama.school.nz public Google calendar for 2018</p>
+        {items}
         <button onClick={this.props.onCancel.bind(this)}>
           Cancel
         </button>
