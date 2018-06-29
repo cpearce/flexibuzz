a582ca38cb7a71408b612cac19d402049174a33a
Chris Pearce <cpearce@mozilla.com>
2018-06-30 09:07:10 +1200

    Add ability to chose dates and calendar to import.

--- src/GoogleCalendarImport.js
+++ src/GoogleCalendarImport.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
-async function getGCal(calendarId) {
+async function getGCal(calendarId, startDate, endDate) {
+  console.log("getGCal " + startDate + " " + endDate);
   const api = "https://www.googleapis.com/calendar/v3/calendars/"
   let url =
     api + calendarId + "/events?"
     + "key=AIzaSyBqhqI543V2ejZi6B_CniqJ7eQYq-iW0wE"
-    + "&timeMin=2018-01-01T00:00:00.000Z&maxResults=250"
-    + "&timeMax=2019-01-01T00:00:00.000Z";
+    + "&timeMin=" + startDate + "T00:00:00.000Z&maxResults=250"
+    + "&timeMax=" + endDate + "T00:00:00.000Z"
+    + "&singleEvents=true";
     let nextPageToken = undefined;
     let calendar = [];
     while (true) {
@@ -26,13 +28,32 @@ async function getGCal(calendarId) {
     return calendar;
 }
 
+const CONFIG = 0;
+const LOADING = 1;
+
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
+
 export class GoogleCalendarImport extends Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [],
+      screen: CONFIG,
+      googleCalendarId: localStorage.getItem("googleCalendarId"),
+      startDate: "",
+      endDate: "",
     };
-    getGCal('office%40kohimarama.school.nz')
+  }
+
+  loadGCal() {
+    this.setState({
+      screen: LOADING,
+    });
+    getGCal(this.state.googleCalendarId,
+            this.state.startDate,
+            this.state.endDate)
     .then((data) => {
       console.log(data);
       let items = data.reduce((accumulator, value) => {
@@ -46,22 +67,104 @@ export class GoogleCalendarImport extends Component {
       console.log(err);
     });
   }
-  render() {
-    let items = this.state.items.map((item) => {
-      return (
-        <div key={item.id} className="gcal-item">
-          {item.start.date} - {item.end.date} ; {item.summary}
-        </div>
-      );
+
+  onStartDateChange(event) {
+    this.setState({
+      startDate: event.target.value,
     });
+  }
+
+  onEndDateChange(event) {
+    this.setState({
+      endDate: event.target.value,
+    });
+  }
+
+  onGoogleCalendarIdChange(event) {
+    const googleCalendarId = event.target.value;
+    this.setState({
+      googleCalendarId: googleCalendarId,
+    });
+    localStorage.setItem("googleCalendarId", googleCalendarId);
+  }
+
+  renderConfigScreen() {
+    const startDate = new Date(this.state.startDate);
+    const endDate = new Date(this.state.endDate);
+    const validInput =
+      isValidDate(startDate) &&
+      isValidDate(endDate) &&
+      startDate.getTime() <= endDate.getTime() &&
+      this.state.googleCalendarId !== "";
     return (
       <div>
-        <p>office@kohimarama.school.nz public Google calendar for 2018</p>
+        <h3>Load Google Calendar for import</h3>
+        Google calendar ID:
+        <input
+          type="text"
+          value={this.state.googleCalendarId}
+          onChange={this.onGoogleCalendarIdChange.bind(this)}
+        />
+        <br />
+        Import events on or after:
+        <input
+          type="date"
+          value={this.state.startDate}
+          onChange={this.onStartDateChange.bind(this)}
+        />
+        <br />
+        up until and including
+        <input
+          type="date"
+          value={this.state.endDate}
+          onChange={this.onEndDateChange.bind(this)}
+        />
+        <br />
+        <button
+          onClick={this.loadGCal.bind(this)}
+          disabled={!validInput}
+        >
+          Load Google Calender Events
+        </button>
+        <button onClick={this.props.onCancel.bind(this)}>
+         Cancel
+       </button>
+      </div>
+    );
+  }
+
+  renderLoadingScreen() {
+    let items;
+    if (this.state.items.length === 0) {
+      items = (
+        <div>
+          Loading...
+        </div>
+      );
+    } else {
+      items = this.state.items.map((item) => {
+        return (
+          <div key={item.id} className="gcal-item">
+            {item.start.date} - {item.end.date} ; {item.summary}
+          </div>
+        );
+      });
+    }
+    return (
+      <div>
         {items}
         <button onClick={this.props.onCancel.bind(this)}>
           Cancel
         </button>
       </div>
     );
+  }
+
+  render() {
+    if (this.state.screen === CONFIG) {
+      return this.renderConfigScreen();
+    } else if (this.state.screen === LOADING) {
+      return this.renderLoadingScreen();
+    }
   }
 }
